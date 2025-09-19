e2f9d6f7743d1ab1fc70cbd49bde5801e2d2bf7a
AnusuyaMuthu <anu014634@gmail.com>
2025-09-19 02:54:07 -0700

    PR crud funcionality added

--- src/app/purchases/purchase-requisition/purchase-requisition.service.ts
+++ src/app/purchases/purchase-requisition/purchase-requisition.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PurchaseAPIUrls } from 'src/app/Urls/PurchaseAPIUrls';
+
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PurchaseRequisitionService {
+private url = environment.apiUrl
+  constructor(private http: HttpClient) { }
+   getAll(): Observable<any> {
+    return this.http.get<any[]>(this.url + PurchaseAPIUrls.GetAllPurchaseRequests);
+  }
 
-  constructor() { }
+  // POST
+  create(data: any): Observable<any> {
+    return this.http.post(this.url+PurchaseAPIUrls.CreatePurchaseRequest, data);
+  }
+
+  // PUT
+update(id: number, data: any): Observable<any> {
+  return this.http.put(`${this.url + PurchaseAPIUrls.UpdatePurchaseRequest}${id}`, data);
+}
+
+  // DELETE
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${this.url + PurchaseAPIUrls.DeletePurchaseRequest}${id}`);
+  }
 }
